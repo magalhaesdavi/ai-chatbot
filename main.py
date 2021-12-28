810740fbbd694539d6fd4ddc6f482e5d8f26b52d
DaviMagalhaes <magalhaesp.davi@gmail.com>
2021-12-27 22:40:48 -0300

    feat: add chat
    Add chat.
    
    Organize code.

--- main.py
+++ main.py
@@ -1,69 +1,53 @@
-import nltk
-from nltk.stem.lancaster import LancasterStemmer
-import numpy
 import torch
 import random
 import json
-from torch.utils.data import TensorDataset, DataLoader
 import model
-
-stemmer = LancasterStemmer()
-
-with open('json file/intents.json') as file:
-    data = json.load(file)
-
-words = []
-labels = []
-docs_x = []
-docs_y = []
-
-for intent in data['intents']:
-    for pattern in intent['patterns']:
-        wrds = nltk.word_tokenize(pattern)
-        words.extend(wrds)
-        docs_x.append(wrds)
-        docs_y.append(intent["tag"])
-
-    if intent['tag'] not in labels:
-        labels.append(intent['tag'])
-
-words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-words = sorted(list(set(words)))
-
-labels = sorted(labels)
-
-training = []
-output = []
-
-out_empty = [0 for _ in range(len(labels))]
-
-for x, doc in enumerate(docs_x):
-    bag = []
-
-    wrds = [stemmer.stem(w.lower()) for w in doc]
-
-    for w in words:
-        if w in wrds:
-            bag.append(1)
-        else:
-            bag.append(0)
-
-    output_row = out_empty[:]
-    output_row[labels.index(docs_y[x])] = 1
-
-    training.append(bag)
-    output.append(output_row)
+from nltk_utils import bag_of_words, tokenize
+import torch.nn as nn
 
 
-training = numpy.array(training)
-output = numpy.array(output)
-
-train_set = TensorDataset(torch.Tensor(training), torch.Tensor(output))
-train_loader = DataLoader(train_set, batch_size=4)
-
-# Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-learning_rate = 0.001
 
-net = model.NeuralNet(len(training[0]), len(output[0])).to(device)
-model.train(net, train_loader, 1000, device, learning_rate)
+with open('data/intents.json', 'r') as json_data:
+    intents = json.load(json_data)
+
+path = "saved/data.pth"
+data = torch.load(path)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = model.NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name = "Billy"
+print(f"Start talking with {bot_name} (type quit to exit)!")
+
+while True:
+    sentence = input("You: ")
+    if sentence == "quit":
+        break
+
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    softmax = nn.Softmax(dim=1)
+    output = softmax(output)
+    prob, prediction = torch.max(output.data, 1)
+    tag = tags[prediction.item()]
+
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                print(f"{bot_name}: {random.choice(intent['responses'])}")
+    else:
+        print(f"{bot_name}: Entendi nada.")
+
--- model.py
+++ model.py
@@ -1,16 +1,14 @@
-import torch
 import torch.nn as nn
 
 
 # Fully connected neural network with one hidden layer
 class NeuralNet(nn.Module):
-    def __init__(self, input_size, num_classes):
+    def __init__(self, input_size, hidden_size, num_classes):
         super(NeuralNet, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, num_classes)
         self.relu = nn.ReLU()
-        self.softmax = nn.Softmax(dim=1)
-        self.fc1 = nn.Linear(input_size, 23)
-        self.fc2 = nn.Linear(23, 11)
-        self.fc3 = nn.Linear(11, num_classes)
 
     def forward(self, x):
         out = self.fc1(x)
@@ -18,52 +16,6 @@ class NeuralNet(nn.Module):
         out = self.fc2(out)
         out = self.relu(out)
         out = self.fc3(out)
-        out = self.softmax(out)
         return out
 
-
-# Train the model
-def train(model, train_loader, num_epochs, device, learning_rate):
-    # Loss and optimizer
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-    total_step = len(train_loader)
-    for epoch in range(num_epochs):
-        for i, (inputs, labels) in enumerate(train_loader):
-            # Move tensors to the configured device
-            inputs = inputs.to(device)
-            labels = labels.to(device)
-
-            # Forward pass
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-
-            # Backward and optimize
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            if (i + 1) % 2 == 0:
-                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-                      .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
-    # Save the model checkpoint
-    torch.save(model.state_dict(), 'weights/model.ckpt')
-
-
-# Test the model
-# In test phase, we don't need to compute gradients (for memory efficiency)
-def test(model, test_loader, device):
-    with torch.no_grad():
-        correct = 0
-        total = 0
-        for images, labels in test_loader:
-            images = images.reshape(-1, 28 * 28).to(device)
-            labels = labels.to(device)
-            outputs = model(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-
-        print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
 
